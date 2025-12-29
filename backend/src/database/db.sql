79f68b85ec00d6b16e101cc4c3546e5f0f9db12f
Boris Espinosa <borisem.dev@gmail.com>
2025-12-28 23:33:15 -0300

    added functions and triggers to database

--- backend/src/database/db.sql
+++ backend/src/database/db.sql
@@ -1,6 +1,10 @@
-CREATE TYPE tasks_status AS ENUM ('PENDING', 'IN_PROGRESS', 'COMPLETED');
-CREATE TYPE task_event_status AS ENUM('CREATED', 'IN_PROGRESS', 'BLOCKED', 'COMPLETED', 'CANCELLED');
 
+--TYPES
+CREATE TYPE tasks_status AS ENUM ('PENDING', 'IN_PROGRESS', 'COMPLETED');
+CREATE TYPE task_event_status AS ENUM('CREATED', 'IN_PROGRESS', 'BLOCKED', 'COMPLETED', 'CANCELLED', 'DELAYED');
+
+
+--TABLES
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -57,3 +61,111 @@ CREATE TABLE task_events (
     FOREIGN KEY (task_id) REFERENCES tasks (id)
         ON DELETE CASCADE
 );
+
+
+-- FUNCTIONS
+CREATE OR REPLACE FUNCTION task_created()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO task_events(task_id, comment, status)
+    VALUES (
+        NEW.id,
+        'Task created',
+        'CREATED'
+    );
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION log_task_status_change()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF OLD.status IS DISTINCT FROM NEW.status THEN
+        INSERT INTO task_events(task_id, comment, status)
+        VALUES (
+            NEW.id,
+            'status changed',
+            NEW.status
+        );
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION set_default_assigned_user()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.assigned_to_id IS NULL THEN
+        NEW.assigned_to_id := NEW.created_by_id;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION set_actual_end_time()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.status = 'COMPLETED'
+       AND OLD.status IS DISTINCT FROM 'COMPLETED' THEN
+        NEW.actual_end_time := CURRENT_TIMESTAMP;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION set_delayed_task_event()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF
+        NEW.expected_end_time IS NOT NULL
+        AND NEW.expected_end_time < NOW()
+        AND (OLD.expected_end_time IS NULL OR OLD.expected_end_time >= NOW())
+        AND NEW.status IS DISTINCT FROM 'COMPLETED'
+    THEN
+        INSERT INTO task_events (task_id, comment, status)
+        VALUES (
+            NEW.id,
+            'task delayed',
+            'DELAYED'
+        );
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- TRIGGERS
+CREATE TRIGGER on_task_insert
+AFTER INSERT ON tasks
+FOR EACH ROW
+EXECUTE FUNCTION task_created();
+
+
+CREATE TRIGGER on_task_update
+AFTER UPDATE ON tasks
+FOR EACH ROW
+EXECUTE FUNCTION log_task_status_change();
+
+
+CREATE TRIGGER set_default_assigned_user_trigger
+BEFORE INSERT ON tasks
+FOR EACH ROW
+EXECUTE FUNCTION set_default_assigned_user();
+
+
+CREATE TRIGGER set_actual_end_time_trigger
+BEFORE UPDATE ON tasks
+FOR EACH ROW
+EXECUTE FUNCTION set_actual_end_time();
+
+CREATE TRIGGER set_delayed_task_event_trigger
+AFTER UPDATE ON tasks
+FOR EACH ROW
+EXECUTE FUNCTION set_delayed_task_event();
